9727e4c2fe1cd0e44ce60051390ba54574a571a0
Dangminhquang99 <Minhquang7274@gmail.com>
2026-03-28 23:51:25 +0700

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -4,11 +4,32 @@ import Gfile as gf
 
 def initial_info():
     tendz_map=gf.tendz_map
-    TimeF=st.text_input("🔢 Thời gian sự cố",placeholder="00:00:00 ngày 01/01/2025")
+    TimeF=st.text_input("⏰ Thời gian sự cố",placeholder="00:00:00 ngày 01/01/2025")
     select_dz=gf.select_name_dz(tendz_map)
-    PhaF = st.pills("Pha sự cố:", ["A-N", "B-N", "C-N", "AB", "BC", "AC"])
+    subs=gf.select_tba_1(select_dz)
+    df = gf.accum(select_dz)
+    phaF = st.pills("Pha sự cố:", ["A-B-C","A-N", "B-N", "C-N", "AB", "BC", "AC"])
     F79 = st.pills("Tình trạng đóng lặp lại:", ["Thành công", "Không thành công", "Không đóng lặp lại"])
-    return F79, TimeF, select_dz, PhaF
+    isc= st.number_input(f"Dòng sự cố:", min_value=0)
+
+    st.subheader(f"📍{subs[0]}")
+    colm1, colm2, colm3=st.columns(3)
+    with colm1: dis87 = st.number_input(f"📏 Nhập khoảng cách sự cố F87/{subs[0]}:", min_value=0,step=500)
+    with colm2: dis21 = st.number_input(f"📏 Nhập khoảng cách sự cố F21/{subs[0]}:", min_value=0,step=500)
+    with colm3: disFL = st.number_input(f"📏 Nhập khoảng cách sự cố FL/{subs[0]}:", min_value=0,step=500)
+
+
+    st.subheader(f"📍{subs[1]}")
+    colm4, colm5, colm6=st.columns(3)
+    with colm4: dis87N1 = st.number_input(f"📏 Nhập khoảng cách sự cố F87/{subs[1]}:", min_value=0,step=500)
+    with colm5: dis21N1 = st.number_input(f"📏 Nhập khoảng cách sự cố F21/{subs[1]}:", min_value=0,step=500)
+    with colm6: disFLN1 = st.number_input(f"📏 Nhập khoảng cách sự cố FL/{subs[1]}:", min_value=0,step=500)
+    st.write(f" 📡***Tổng chiều dài đường dây: {df['Chiều dài'].sum()} m***")
+
+    return TimeF, select_dz, phaF, F79, isc, dis87, dis21, disFL, dis87N1, dis21N1, disFLN1,subs, df
+
+
+
 
 def find_positions(value, data,role_name,subs_no):
     lower=0     #Khoi tao lower bang 0
@@ -34,20 +55,18 @@ def find_positions(value, data,role_name,subs_no):
                 result = data['Vị trí'].iloc[i-1],data['Vị trí'].iloc[i], data['Vị trí'].iloc[i + 1]   
             elif min(lower, upper) < value <= max(lower, upper):
                 result = data['Vị trí'].iloc[i - 1],data['Vị trí'].iloc[i]
-        
+    return result, value    
                 
+
+def info(df, result, role_name, value, subs):
     if value:
         if not result:
             st.warning(f"⚠️ Không tìm thấy khoảng phù hợp với giá trị đã nhập, hãy đảm bảo rằng giá trị nằm trong phạm vi chiều dài của đường dây! ")
         elif len(result)==2:
-            st.info(f"🔍 Khoảng cách rơ le {role_name} tương đương khoảng cột {result[0]} - {result[1]}.")
+            st.info(f"🔍 Khoảng cách rơ le {role_name}/{subs} tương đương khoảng cột {result[0]} - {result[1]}.")
         elif len(result)==3:
-            st.info(f"🔍 Khoảng cách rơ le {role_name} tương đương khoảng cột {result[0]} - {result[1]} - {result[2]}.")          
-        return result
-    return None
+            st.info(f"🔍 Khoảng cách rơ le {role_name}/{subs} tương đương khoảng cột {result[0]} - {result[1]} - {result[2]}.")          
 
-
-def info(df, result, role_name):
     if not result:
         st.warning(f"⚠️ Không có dữ liệu kết quả để hiển thị cho {role_name}.")
         return
@@ -57,33 +76,21 @@ def info(df, result, role_name):
     get_col=df.query("`Vị trí` in @result")[['Vị trí','Khoảng cột', 'Công dụng cột','Thứ tự pha','Chiều dài']]
     st.write(get_col.T)
 
-
-def process(subs, df, subs_no):
-    st.header(f"📋{subs}")
-    st.write(f" 🔍***Tổng chiều dài đường dây: {df['Chiều dài'].sum()} m***")
-    st.markdown("---")
-    dis87 = st.number_input(f"🔢 Nhập khoảng cách sự cố F87/{subs}:", min_value=0,step=500)
-    dis21 = st.number_input(f"🔢 Nhập khoảng cách sự cố F21/{subs}:", min_value=0,step=500)
-    result_87 = find_positions(dis87, df, "F87", subs_no)
-    if result_87:
-        info(df, result_87, "F87")
-        directions (df, result_87)
-    result_21 = find_positions(dis21, df, "F21", subs_no)
-    if result_21:
-        info(df, result_21, "F21")
-        directions (df, result_21)
-    return dis87, dis21, result_87, result_21
+    #Định vị trên bản đồ
+    lat=df.query("`Vị trí` in @result")['Latitude']
+    lon=df.query("`Vị trí` in @result")['Longitude']
+    maps_url_0 = f"https://www.google.com/maps?q={lat.iloc[0]},{lon.iloc[0]}"
+    st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[0]}]({maps_url_0})", unsafe_allow_html=True)
+    maps_url_1 = f"https://www.google.com/maps?q={lat.iloc[1]},{lon.iloc[1]}"
+    st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[1]}]({maps_url_1})", unsafe_allow_html=True)
 
 
- 
-def directions (df, result):
-    if result:
-        lat=df.query("`Vị trí` in @result")['Latitude']
-        lon=df.query("`Vị trí` in @result")['Longitude']
-        maps_url_0 = f"https://www.google.com/maps?q={lat.iloc[0]},{lon.iloc[0]}"
-        st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[0]}]({maps_url_0})", unsafe_allow_html=True)
-        maps_url_1 = f"https://www.google.com/maps?q={lat.iloc[1]},{lon.iloc[1]}"
-        st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[1]}]({maps_url_1})", unsafe_allow_html=True)
-    return None
+def process(subs, df, subs_no, dis87, dis21, disFL):
+
+    result_87, value87 = find_positions(dis87, df, "F87", subs_no)
+    result_21, value21 = find_positions(dis21, df, "F21", subs_no)
+    result_FL, valueFL = find_positions(disFL, df, "FL", subs_no)
+
+    return dis87, dis21, disFL, result_87, result_21, result_FL, value87,value21, valueFL
     
 
