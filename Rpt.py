d38f4a066f8cc9c3ac0ea63247fbd8260f01ed04
Dangminhquang99 <Minhquang7274@gmail.com>
2026-03-28 23:53:02 +0700

    Update Rpt.py

--- Rpt.py
+++ Rpt.py
@@ -5,11 +5,10 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
-
-pdfmetrics.registerFont(TTFont('times', 'times.ttf'))
+pdfmetrics.registerFont(TTFont('times', 'fonts/times.ttf'))
 import io
 
-def report (TimeF, phaF, F79, select_dz, subs, dis87, dis21, result_87, result_21, dis87_1, dis21_1, result_87_1, result_21_1):
+def report (TimeF, select_dz, phaF, isc, F79, subs, dis87, dis21, disFL, result_87, result_21, result_FL, dis87_1, dis21_1, disFL_1, result_87_1, result_21_1, result_FL_1):
     
     buffer = io.BytesIO()
     doc = SimpleDocTemplate(buffer)
@@ -17,46 +16,65 @@ def report (TimeF, phaF, F79, select_dz, subs, dis87, dis21, result_87, result_2
 
     style = ParagraphStyle(
         name="Vietnamese",
-        fontName="times",
-        fontSize=12
+        fontName="Times-Roman",
+        fontSize=16,
+        leading=24
     )
     content = []
 
     content.append(Paragraph(f"Thời gian: {TimeF}", style))
     content.append(Paragraph(f"Đường dây sự cố: {select_dz}", style))
     content.append(Paragraph(f"Pha sự cố: {phaF}", style))
+    content.append(Paragraph(f"Dòng sự cố Isc: {isc}", style))
     content.append(Paragraph(f"Tình trạng đóng lặp lại: {F79}", style))
-    if not dis21 and not dis87:
-        content.append(Paragraph(f"***{subs[0]} :No data", style))
-    else:
-        content.append(Paragraph(f"{subs[0]}", style))
-        if dis87:
-            content.append(Paragraph(f"     - Khoảng cách F87: {dis87}m <=> tương ứng khoảng cột {result_87[0]}-{result_87[1]}", style))
-        else:
-            content.append(Paragraph(f"     - Khoảng cách F87: No data", style))
-        if dis21:
-            content.append(Paragraph(f"     - Khoảng cách F21: {dis21}m <=> tương ứng khoảng cột {result_21[0]}-{result_21[1]}", style))
-        else:
-            content.append(Paragraph(f"     - Khoảng cách F21: No data", style))
+    
 
-    if not dis21_1 and not dis87_1:
-        content.append(Paragraph(f"{subs[1]} :No data", style))
+    data = [
+        ("Khoảng cách F87", dis87, result_87),
+        ("Khoảng cách F21", dis21, result_21),
+        ("Khoảng cách FL", disFL, result_FL),
+    ]
+
+    if not any([dis21, dis87, disFL]):
+        content.append(Paragraph(f"*{subs[0]} : No data", style))
     else:
-        content.append(Paragraph(f"***{subs[1]}", style))
-        if dis87_1:
-            content.append(Paragraph(f"     - Khoảng cách F87: {dis87_1}m <=> tương ứng khoảng cột {result_87_1[0]}-{result_87_1[1]}", style))
+        content.append(Paragraph(f"*{subs[0]}", style))
+
+        for label, dis, result in data:
+            if dis:
+                text = f"     - {label}: {dis}m <=> tương ứng khoảng cột {result[0]}-{result[1]}"
+            else:
+                text = f"     - {label}: No data"
+
+            content.append(Paragraph(text, style))
+
+        data_1 = [
+            ("Khoảng cách F87", dis87_1, result_87_1),
+            ("Khoảng cách F21", dis21_1, result_21_1),
+            ("Khoảng cách FL", disFL_1, result_FL_1),
+        ]
+
+        if not any([dis21_1, dis87_1, disFL_1]):
+            content.append(Paragraph(f"*{subs[1]} : No data", style))
         else:
-            content.append(Paragraph(f"     - Khoảng cách F87: No data", style))
-        if dis21_1:
-            content.append(Paragraph(f"     - Khoảng cách F21: {dis21_1}m <=> tương ứng khoảng cột {result_21_1[0]}-{result_21_1[1]}", style))
-        else:
-            content.append(Paragraph(f"     - Khoảng cách F21: No data", style))
+            content.append(Paragraph(f"*{subs[1]}", style))
+
+            for label, dis, result in data_1:
+                if dis:
+                    text = f"     - {label}: {dis}m <=> tương ứng khoảng cột {result[0]}-{result[1]}"
+                else:
+                    text = f"     - {label}: No data"
+
+                content.append(Paragraph(text, style))
+
+
     doc.build(content)
 
 
     st.download_button(
     label="📥 Download PDF",
     data=buffer,
-    file_name="sum_report.pdf",
-    mime="application/pdf")
-    return None   
+    file_name="Fault_info.pdf",
+    mime="application/pdf",
+    width="stretch")
+    return None  
