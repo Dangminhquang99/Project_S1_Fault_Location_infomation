27f0892180298a91263986114d8e0a2737910baf
Dangminhquang99 <Minhquang7274@gmail.com>
2026-04-09 08:51:56 +0700

    Update Algo.py

--- Algo.py
+++ Algo.py
@@ -4,13 +4,15 @@ import Gfile as gf
 
 def initial_info():
     tendz_map=gf.tendz_map
-    TimeF=st.text_input("⏰ Thời gian sự cố",placeholder="00:00:00 ngày 01/01/2025")
+    TimeF=st.text_input("⏰ Thời gian sự cố",placeholder="00 giờ 00 phút 00, ngày 01 tháng 01 năm 2025")
     select_dz=gf.select_name_dz(tendz_map)
     subs=gf.select_tba_1(select_dz)
     df = gf.accum(select_dz)
-    phaF = st.pills("Pha sự cố:", ["A-B-C","A-N", "B-N", "C-N", "AB", "BC", "AC"])
-    F79 = st.pills("Tình trạng đóng lặp lại:", ["Thành công", "Không thành công", "Không đóng lặp lại"])
+    phaF = st.pills("Pha sự cố:", ["A-B-C","A-N", "B-N", "C-N", "AB", "BC", "AC"],width=600)
+    F79 = st.pills("Tình trạng đóng lặp lại:", ["Thành công", "Không thành công", "Không đóng lặp lại"],width=600)
+    weather= st.pills("Thời tiết:", ["Không mưa", "Mưa", "Mưa kèm giông sét"],width=600)
     isc= st.number_input(f"Dòng sự cố:", min_value=0)
+    
 
     st.subheader(f"📍{subs[0]}")
     colm1, colm2, colm3=st.columns(3)
@@ -26,7 +28,9 @@ def initial_info():
     with colm6: disFLN1 = st.number_input(f"📏 Nhập khoảng cách sự cố FL/{subs[1]}:", min_value=0,step=500)
     st.write(f" 📡***Tổng chiều dài đường dây: {df['Chiều dài'].sum()} m***")
 
-    return TimeF, select_dz, phaF, F79, isc, dis87, dis21, disFL, dis87N1, dis21N1, disFLN1,subs, df
+    comment = st.text_area("Ghi chú:",height=1)
+
+    return TimeF, select_dz, phaF, F79, isc, weather, dis87, dis21, disFL, dis87N1, dis21N1, disFLN1,subs, df, comment
 
 
 
@@ -67,22 +71,22 @@ def info(df, result, role_name, value, subs):
         elif len(result)==3:
             st.info(f"🔍 Khoảng cách rơ le {role_name}/{subs} tương đương khoảng cột {result[0]} - {result[1]} - {result[2]}.")          
 
-    if not result:
-        st.warning(f"⚠️ Không có dữ liệu kết quả để hiển thị cho {role_name}.")
-        return
-    if 'Vị trí' not in df.columns:
-        st.error("❌ File thông tin cột thiếu cột 'Vị trí'.")
-        return
-    get_col=df.query("`Vị trí` in @result")[['Vị trí','Khoảng cột', 'Công dụng cột','Thứ tự pha','Chiều dài']]
-    st.write(get_col.T)
+        if not result:
+            st.warning(f"⚠️ Không có dữ liệu kết quả để hiển thị cho {role_name}.")
+            return
+        if 'Vị trí' not in df.columns:
+            st.error("❌ File thông tin cột thiếu cột 'Vị trí'.")
+            return
+        get_col=df.query("`Vị trí` in @result")[['Vị trí','Khoảng cột', 'Công dụng cột','Thứ tự pha','Chiều dài']]
+        st.write(get_col.T)
 
-    #Định vị trên bản đồ
-    lat=df.query("`Vị trí` in @result")['Latitude']
-    lon=df.query("`Vị trí` in @result")['Longitude']
-    maps_url_0 = f"https://www.google.com/maps?q={lat.iloc[0]},{lon.iloc[0]}"
-    st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[0]}]({maps_url_0})", unsafe_allow_html=True)
-    maps_url_1 = f"https://www.google.com/maps?q={lat.iloc[1]},{lon.iloc[1]}"
-    st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[1]}]({maps_url_1})", unsafe_allow_html=True)
+        #Định vị trên bản đồ
+        lat=df.query("`Vị trí` in @result")['Latitude']
+        lon=df.query("`Vị trí` in @result")['Longitude']
+        maps_url_0 = f"https://www.google.com/maps?q={lat.iloc[0]},{lon.iloc[0]}"
+        st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[0]}]({maps_url_0})", unsafe_allow_html=True)
+        maps_url_1 = f"https://www.google.com/maps?q={lat.iloc[1]},{lon.iloc[1]}"
+        st.markdown(f"[🗺️ Xem trên bản đồ: VT {result[1]}]({maps_url_1})", unsafe_allow_html=True)
 
 
 def process(subs, df, subs_no, dis87, dis21, disFL):
